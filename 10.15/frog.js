74d2d0895e7f737dacc2e6f861abb7b31c7e2285
seth23michel <seth23michel@gmail.com>
2023-04-15 18:31:30 -0400

    new file:   10.14/index.html 	new file:   10.14/snowman.js 	new file:   10.15/frog.js 	new file:   10.15/frog.png 	new file:   10.15/index.html

---
+++ 10.15/frog.js
@@ -0,0 +1,41 @@
+const directions = {
+   UP: 'up',
+   DOWN: 'down',
+   LEFT: 'left',
+   RIGHT: 'right'
+}
+
+window.addEventListener("load", function () {
+   const canvas = document.querySelector("canvas");
+
+   // Draw 4 frogs facing different directions
+   drawFrog(canvas, 50, 50, directions.UP);
+   drawFrog(canvas, 180, 50, directions.DOWN);   
+   drawFrog(canvas, 50, 180, directions.LEFT);
+   drawFrog(canvas, 180, 180, directions.RIGHT);
+});
+
+function drawFrog(canvas, x, y, direction = directions.UP) {   
+   const context = canvas.getContext("2d");
+   const frogImg = document.querySelector("img");
+
+   switch (direction) {
+      case directions.DOWN:
+         // TODO: Translate, rotate, and translate
+         
+         break;
+      case directions.LEFT:
+         // TODO: Translate, rotate, and translate
+                  
+         break;
+      case directions.RIGHT:
+         // TODO: Translate, rotate, and translate
+         
+         break;
+   }
+
+   context.drawImage(frogImg, x, y);
+
+   // Necessary so next call to drawFrog isn't rotated or translated
+   context.resetTransform();
+}
